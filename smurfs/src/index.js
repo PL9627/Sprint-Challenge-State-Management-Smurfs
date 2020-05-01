f0edc726c12b2deff442dfeb72688999af02cd09
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:21:37 -0700

    created store, wrapped App in Provider, imported smurfReducer into index.js, and created smurfReducer skeleton

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import smurfReducer from './reducers/smurfReducer';
+
+const store = createStore(smurfReducer);
+
+ReactDOM.render(<Provider store={store}>
+    <App /></Provider>, document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+
+};
+
+const smurfReducer = (state = initialState, action) => {
+
+};
+
+export default smurfReducer;
