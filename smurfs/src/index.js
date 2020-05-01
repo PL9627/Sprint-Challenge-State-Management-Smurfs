d00de643c3f7fd25c7cfef137c4df71025f7fcdd
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:23:32 -0700

    set up switch statement skeleton

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import smurfReducer from './reducers/smurfReducer';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import smurfReducer from "./reducers/smurfReducer";
 
 const store = createStore(smurfReducer);
 
-ReactDOM.render(<Provider store={store}>
-    <App /></Provider>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -3,7 +3,10 @@ const initialState = {
 };
 
 const smurfReducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        default:
+            return state;
+    }
 };
 
 export default smurfReducer;
