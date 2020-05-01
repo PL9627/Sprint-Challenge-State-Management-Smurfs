0199624ca8dc62f7fa043b3a9afbc25e49506b8c
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:41:28 -0700

    imported actions into smurfReducer

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,12 +1,12 @@
-const initialState = {
+import { GET_SMURF, OBTAIN_SMURF } from "../actions/smurfActions";
 
-};
+const initialState = {};
 
 const smurfReducer = (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
 export default smurfReducer;
