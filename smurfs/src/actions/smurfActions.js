97dfd48d5403b5c0b372a463d28c0a87d9f43c6e
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:37:16 -0700

    created obtainSmurf

--- smurfs/src/actions/smurfActions.js
+++ smurfs/src/actions/smurfActions.js
@@ -1,9 +1,16 @@
-const OBTAIN_SMURF = 'OBTAIN_SMURF';
-const GET_SMURF = 'GET_SMURF';
+const OBTAIN_SMURF = "OBTAIN_SMURF";
+const GET_SMURF = "GET_SMURF";
 
 export const obtainSmurf = (res) => {
-    return {
-        type: OBTAIN_SMURF,
-        payload: res
-    }
-}
+  return {
+    type: OBTAIN_SMURF,
+    payload: res,
+  };
+};
+
+export const getSmurf = (props) => {
+  return {
+    type: GET_SMURF,
+    payload: props,
+  };
+};
