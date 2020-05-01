98e147ff15acf924e7cda5a1071936805a5f1f69
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:48:00 -0700

    created getForm skeleton

--- smurfs/src/forms/getForms.js
+++ smurfs/src/forms/getForms.js
@@ -2,3 +2,9 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import { getSmurf } from "../actions/smurfActions";
+
+const getForm = (props) => {
+  return <div></div>;
+};
+
+export default getForm;
