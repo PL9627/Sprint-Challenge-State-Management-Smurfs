69dc60a863ae8b043b3f537094d3fa5d701e7105
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:42:58 -0700

    added initialState data in smurfReducer

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,6 +1,12 @@
 import { GET_SMURF, OBTAIN_SMURF } from "../actions/smurfActions";
 
-const initialState = {};
+const initialState = [
+  {
+    name: "",
+    age: 0,
+    height: "0cm",
+  },
+];
 
 const smurfReducer = (state = initialState, action) => {
   switch (action.type) {
