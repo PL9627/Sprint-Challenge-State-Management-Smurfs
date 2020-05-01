25e1e48968daaa5a69447fc9cb694e21062977d6
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 16:46:03 -0700

    added useEffect in getForm

--- smurfs/src/forms/getForms.js
+++ smurfs/src/forms/getForms.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import { getSmurf } from "../actions/smurfActions";
