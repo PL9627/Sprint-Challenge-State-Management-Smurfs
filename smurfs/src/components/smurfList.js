2c214ef73f6275c5649d867a41b90470fa20a6c3
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 20:03:41 -0700

    finished with smurfList return statement

--- smurfs/src/components/smurfList.js
+++ smurfs/src/components/smurfList.js
@@ -1,13 +1,19 @@
-import React, {useContext, useState} from 'react';
-import {SmurfContext} from '../smurfContext/smurfContext';
-import {SmurfCard} from './SmurfCard';
+import React, { useContext, useState } from "react";
+import { SmurfContext } from "../smurfContext/smurfContext";
+import { SmurfCard } from "./SmurfCard";
 
 const SmurfList = () => {
-    const {smurfs} = useContext(SmurfContext);
-   
-    return (
-        <div className="smurfList">
-            
-        </div>
-    )
-}
+  const { smurfs } = useContext(SmurfContext);
+
+  return (
+    <div className="smurfList">
+      <h1>Smurf Form</h1>
+
+      <div>
+        {smurfs.map((smurf) => {
+          <SmurfCard key={smurf.id} smurf={smurf} />;
+        })}
+      </div>
+    </div>
+  );
+};
