12cf932901f80ed1dd980d1ddc96a9dbf6988541
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 17:08:11 -0700

    added useState to react import statement

--- smurfs/src/forms/obtainForm.js
+++ smurfs/src/forms/obtainForm.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import { obtainSmurf } from "../actions/smurfActions";
 
 const obtainForm = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default obtainForm;
