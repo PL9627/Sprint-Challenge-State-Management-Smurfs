94ba435bc53baec520192f5c3f8cdf585e68ad81
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 19:20:12 -0700

    added context files

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import ObtainForm from "../forms/obtainForm";
-import GetForm from "../forms/getForms";
-
 class App extends Component {
   render() {
     return (
@@ -12,12 +9,7 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        <div>
-          <GetForm />
-        </div>
-        <div>
-          <ObtainForm />
-        </div>
+        
       </div>
     );
   }
