c313726e0268d059ba559f18c63f44f6af7f730d
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 19:30:41 -0700

    started on list. might repurpose and add more files

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -9,7 +9,6 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        
       </div>
     );
   }
