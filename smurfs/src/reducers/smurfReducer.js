14d2d07584e65eee85471d1eae0cf69e82386ca9
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-01 18:06:07 -0700

    finished with cases for smurfREducer

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -10,10 +10,16 @@ const initialState = [
 
 const smurfReducer = (state = initialState, action) => {
   switch (action.type) {
-      case OBTAIN_SMURF:
-          return {
-            ...state, smurfs: action.payload
-          };
+    case OBTAIN_SMURF:
+      return {
+        ...state,
+        smurfs: action.payload,
+      };
+    case GET_SMURF:
+      return {
+        ...state,
+        smurfs: action.payload,
+      };
     default:
       return state;
   }
